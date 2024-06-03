076434590c53baec89074f79cba34d852fade3a1
gcharlesnicolai <gcharlesnicolai@GCFRLY369>
2024-06-03 16:34:38 +0200

    update : added graphics

--- ConsoleApplication1/Graphics.cpp
+++ ConsoleApplication1/Graphics.cpp
@@ -1,1 +1,29 @@
 #include "Graphics.h"
+#include "Global.h"
+#include "Render.h"
+#include "Mesh.h"
+#include "Shader.h"
+//#include "Texture2.h"
+Graphics::Graphics() {
+
+}
+
+void Graphics::CreateMesh(int type) {
+	//Mesh* mesh = new Mesh();
+	//mesh->Initialize(type);
+	//mMeshes.push_back(mesh);
+}
+
+
+void Graphics::CreateShader() {
+	/*mShaders.push_back(new Shader());*/
+}
+
+//void Graphics::CreateTexture() {
+//	mTextures.push_back(new Texture2());
+//}
+
+//std::vector<Mesh*> Graphics::GetMeshes()
+//{
+//	return mMeshes;
+//}
--- ConsoleApplication1/Graphics.h
+++ ConsoleApplication1/Graphics.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <vector>
+class Texture2;
+class Mesh;
+class Shader;
 class Graphics
 {
+private:
+	std::vector<Mesh*> mMeshes;
+public:
+	Graphics();
+	void CreateMesh(int type);
+	void CreateShader();
+	//void CreateTexture();
+	//std::vector<Mesh*> GetMeshes();
+public:
+	std::vector<Texture2*> mTextures;
+	std::vector<Shader*> mShaders;
 };
 
